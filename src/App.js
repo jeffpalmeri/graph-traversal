ab5166b8d38f21bcb471f47db76d405717936d67
Jeff <jeffpalmeri184@gmail.com>
2020-06-28 21:49:23 -0400

    fix stale closure problem by storing a gridReference

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { useState, useCallback, useRef } from 'react';
 import './App.css';
 
-const numRows = 3;
-const numColumns = 3;
+const numRows = 25;
+const numColumns = 25;
 
 const generateEmptyGrid = () => {
   let grid = [];
@@ -15,37 +15,6 @@ const generateEmptyGrid = () => {
   return grid;
 };
 
-// function getUnivsitedNeighbors(grid, i, j) {
-//   let neigh = [];
-//   if (i + 1 < grid.length && !grid[i + 1][j]) neigh.push([i + 1, j]);
-//   if (i - 1 >= 0 && !grid[i - 1][j]) neigh.push([i - 1, j]);
-//   if (j + 1 < grid[i].length && !grid[i][j + 1]) neigh.push([i, j + 1]);
-//   if (j - 1 >= 0 && !grid[i][j - 1]) neigh.push([i, j - 1]);
-//   return neigh;
-// }
-
-// let testGrid = generateEmptyGrid();
-// testGrid[2][1] = 1;
-// // testGrid[2][3] = 1;
-// testGrid[1][2] = 1;
-// // testGrid[3][2] = 0;
-// console.log('TESTGRID: ', testGrid);
-// // console.log(getUnivsitedNeighbors(testGrid, 2, 2));
-
-// let numberOfZeros = 0;
-// for (let row = 0; row < numRows; row++) {
-//   for (let column = 0; column < numColumns; column++) {
-//     if (testGrid[row][column] === 0) {
-//       numberOfZeros++;
-//     }
-//     console.log('grid[row][column]', grid[row][column]);
-//     console.log('NUMBEROFZEROS', numberOfZeros);
-//   }
-// }
-
-let start = [2, 2];
-const stack = [start];
-const result = [];
 let currentVertex;
 let i;
 let j;
@@ -53,16 +22,11 @@ let j;
 const App = () => {
   const [grid, setGrid] = useState(() => generateEmptyGrid());
   const [running, setRunning] = useState(false);
-  const stack = useRef([[1, 1]]);
-  // console.log('pre-pop', stack.current);
-  // let test = stack.current.pop();
-  // console.log('test', test);
-
-  console.log('rerender*****************');
+  const gridRef = useRef(grid);
+  const startingNode = useRef();
+  const stack = useRef([startingNode.current]);
 
   const runningReference = useRef(running);
-  // runningReference.current = running;
-  // console.log('what is this', runningReference.current);
 
   const runSimulation = useCallback(() => {
     if (!runningReference.current) return;
@@ -70,18 +34,6 @@ const App = () => {
       setRunning(false);
       return;
     }
-
-    // console.log(grid);
-    // let numberOfZeros = 0;
-    // for (let row = 0; row < numRows; row++) {
-    //   for (let column = 0; column < numColumns; column++) {
-    //     if (grid[row][column] === 0) {
-    //       numberOfZeros++;
-    //       console.log(numberOfZeros);
-    //     }
-    //   }
-    //   if (numberOfZeros === 0) return;
-    // }
 
     setGrid((g) => {
       let gridCopy = JSON.parse(JSON.stringify(g));
@@ -91,46 +43,28 @@ const App = () => {
         i = currentVertex[0];
         j = currentVertex[1];
         let univsitedNeighbors = getUnivsitedNeighbors(gridCopy, i, j);
-        console.log(
-          'UNVISITEDnEIGHBORS: ',
-          JSON.parse(JSON.stringify(univsitedNeighbors))
-        );
 
         univsitedNeighbors.forEach((neighbor) => {
           stack.current.push(neighbor);
-          // gridCopy[neighbor[0]][neighbor[1]] = 1;
+          gridCopy[neighbor[0]][neighbor[1]] = 1;
         });
-
-        gridCopy[i][j] = 1;
-        console.log(JSON.parse(JSON.stringify(stack.current)));
-        console.log('current: ', JSON.parse(JSON.stringify(currentVertex)));
       }
 
+      gridRef.current = gridCopy;
       return gridCopy;
     });
 
-    function getUnivsitedNeighbors(grid, i, j) {
-      let neigh = [];
-      if (i + 1 < grid.length && !grid[i + 1][j]) neigh.push([i + 1, j]);
-      if (i - 1 >= 0 && !grid[i - 1][j]) neigh.push([i - 1, j]);
-      if (j + 1 < grid[i].length && !grid[i][j + 1]) neigh.push([i, j + 1]);
-      if (j - 1 >= 0 && !grid[i][j - 1]) neigh.push([i, j - 1]);
-      return neigh;
+    let numberOfZeros = 0;
+    for (let row = 0; row < numRows; row++) {
+      for (let column = 0; column < numColumns; column++) {
+        if (gridRef.current[row][column] === 1) {
+          numberOfZeros++;
+        }
+      }
+      if (numberOfZeros === numRows * numColumns) return setRunning(false);
     }
 
-    console.log(JSON.parse(JSON.stringify(grid)));
-    // let numberOfZeros = 0;
-    // for (let row = 0; row < numRows; row++) {
-    //   for (let column = 0; column < numColumns; column++) {
-    //     if (grid[row][column] === 0) {
-    //       numberOfZeros++;
-    //       console.log(numberOfZeros);
-    //     }
-    //   }
-    //   if (numberOfZeros === 0) return;
-    // }
-
-    setTimeout(runSimulation, 1000);
+    setTimeout(runSimulation, 10);
   }, []);
 
   function getUnivsitedNeighbors(grid, i, j) {
@@ -142,7 +76,13 @@ const App = () => {
     return neigh;
   }
 
-  // console.log(getUnivsitedNeighbors(grid, 5, 5));
+  const resetGrid = () => {
+    runningReference.current = false;
+    setGrid(() => generateEmptyGrid());
+    gridRef.current = grid;
+    stack.current = [];
+    setRunning(false);
+  };
 
   let gridRender = grid.map((row, i) => (
     <div className='row' key={i}>
@@ -152,6 +92,8 @@ const App = () => {
           onClick={() => {
             let gridCopy = JSON.parse(JSON.stringify(grid));
             gridCopy[i][k] = grid[i][k] ? 0 : 1;
+            startingNode.current = [i, k];
+            stack.current = [startingNode.current];
             setGrid(gridCopy);
           }}
           style={{
@@ -181,13 +123,16 @@ const App = () => {
       >
         {running ? 'Stop' : 'Start'}
       </button>
+      <button
+        onClick={() => {
+          resetGrid();
+        }}
+      >
+        Reset
+      </button>
       <div className='grid'>{gridRender}</div>
     </div>
   );
 };
 
 export default App;
-
-//get the neighboring cells
-//change the state of each of those cells to 1
-// add each of those cells to the stack
